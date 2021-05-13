ec093f2f3a2d9048a66523877283adf4626a9cf4
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-05-13 13:24:07 -0700

    added transiton effects and content for about-me section

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,32 @@
+const portfolioEl = document.querySelector(".portfolio")
 var navEl = document.querySelector("#nav");
 var introEl = document.querySelector("#intro");
+var cardContentEl = document.querySelector(".content-card")
 var viewPortfolio = document.querySelector("#view");
 var aboutMeCard = document.querySelector("#about-me");
+var cardImage = document.querySelector(".card-img")
 
 
 viewPortfolio.addEventListener("click", function(){
-    introEl.setAttribute("class","no-display")
-    navEl.setAttribute("class","mb-auto visible")
+    introEl.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;")
+    setTimeout(function(){
+        introEl.setAttribute("class","no-display");
+    navEl.setAttribute("class","mb-auto visible");
+
+    aboutMeCard.setAttribute("style","height: 55%; width: 100%; transition-property: transform; transition-duration: 600ms; transform: translate(0%)");
     aboutMeCard.setAttribute("class","card");
+    var imgEl = document.createElement("img");
+    imgEl.setAttribute("src","./Images/profile-pic (2).jpg");
+    imgEl.setAttribute("alt","Picture of Leigh Dahlin");
+    cardImage.appendChild(imgEl);
+    var paragraphEl = document.createElement("p");
+    paragraphEl.textContent = "My name is Leigh Dahlin and I currently reside in Sacramento, California. I grew up in Houston, Texas and got my bachelor's and master's degrees in Accounting at Texas A&M University in 2014. I worked in the accounting field for seven years when I decided that I needed a career change to something that I was passionate about. That's when I discovered the world of coding and all of the opportunites that came along with it."
+    cardContentEl.appendChild(paragraphEl);
+},650)
+    
+    
 })
+
+// portfolioEl.addEventListener("click", function(){
+//     insert code to lead back to initial page
+// })
