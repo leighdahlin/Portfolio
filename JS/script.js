ab3d61c358ab93616afff32fc2a5f577f31afded
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-05-13 18:39:00 -0700

    fixed work section to pop up

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,7 @@ var cardTitle = document.querySelector(".card-title");
 var workEl = document.querySelector("#work");
 var contactEl = document.querySelector("#contact");
 var resumeEl = document.querySelector("#resume");
+var workSectionEl = document.querySelector("#work-section");
 
 portfolioEl.addEventListener("click", function(){
     window.location = "../index.html"
@@ -33,33 +34,39 @@ viewPortfolio.addEventListener("click", function(){
     var paragraphEl = document.createElement("p");
     paragraphEl.textContent = "My name is Leigh Dahlin and I currently reside in Sacramento, California. I grew up in Houston, Texas and got my bachelor's and master's degrees in Accounting at Texas A&M University in 2014. I worked in the accounting field for seven years when I decided that I needed a career change to something that I was passionate about. That's when I discovered the world of coding and all of the opportunites that came along with it."
     cardContentEl.appendChild(paragraphEl);
+
 },650)
        
 })
 
 workEl.addEventListener("click", function(){
-    aboutMeCard.removeChild(cardBody);
+    aboutMeCard.setAttribute("style","transition-property: transform; transition-duration: 600ms; transform: translate(-150%)");
+    setTimeout(function(){
+        aboutMeCard.setAttribute("class","no-display")
+        workSectionEl.setAttribute("class","visible")
+
+    },300)
 
 
-    var projImg1 = document.createElement("img");
-    projImg1.setAttribute("src","Images/white-plaster (3).jpg")
-    projImg1.setAttribute("class","card-img-top");
-    aboutMeCard.appendChild(projImg1);
+    // var projImg1 = document.createElement("img");
+    // projImg1.setAttribute("src","Images/white-plaster (3).jpg")
+    // projImg1.setAttribute("class","card-img-top");
+    // aboutMeCard.appendChild(projImg1);
 
-    var cardBodyDiv1 = document.createElement("div");
-    cardBodyDiv1.setAttribute("class","card-body");
+    // var cardBodyDiv1 = document.createElement("div");
+    // cardBodyDiv1.setAttribute("class","card-body");
 
-    var cardHeading1 = document.createElement("h5");
-    cardHeading1.setAttribute("class","card-title");
-    cardHeading1.textContent= "The Hopperator";
-    cardBodyDiv1.appendChild(cardHeading1);
+    // var cardHeading1 = document.createElement("h5");
+    // cardHeading1.setAttribute("class","card-title");
+    // cardHeading1.textContent= "The Hopperator";
+    // cardBodyDiv1.appendChild(cardHeading1);
 
-    var cardSubcontent1 = document.createElement("p");
-    cardSubcontent1.setAttribute("class","card-text");
-    cardSubcontent1.textContent = "HTML, CSS, Javascript and Google Maps API";
-    cardBodyDiv1.appendChild(cardSubcontent1);
+    // var cardSubcontent1 = document.createElement("p");
+    // cardSubcontent1.setAttribute("class","card-text");
+    // cardSubcontent1.textContent = "HTML, CSS, Javascript and Google Maps API";
+    // cardBodyDiv1.appendChild(cardSubcontent1);
 
-    aboutMeCard.appendChild(cardBodyDiv1);
+    // aboutMeCard.appendChild(cardBodyDiv1);
 
     
 
