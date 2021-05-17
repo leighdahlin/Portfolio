538b29f31d7f80f2424bbf437450293a0e809902
Leigh Dahlin <leigh.dahlin@gmail.com>
2021-05-17 07:51:16 -0700

    added contact section and fixed transitions

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,12 @@ var introEl = document.querySelector("#intro");
 var cardContentEl = document.querySelector(".content-card")
 var viewPortfolio = document.querySelector("#view");
 var aboutMeCard = document.querySelector("#card1");
+var contactCard = document.querySelector("#card2");
 var cardBody = document.querySelector(".card-body");
 var cardImage = document.querySelector(".card-img");
 var cardTitle = document.querySelector(".card-title");
+var cardTitle2 = document.querySelector(".card-title2");
+var contactEl = document.querySelector(".contact-info");
 var workEl = document.querySelector("#work");
 var contactEl = document.querySelector("#contact");
 var resumeEl = document.querySelector("#resume");
@@ -19,41 +22,44 @@ portfolioEl.addEventListener("click", function(){
 
 viewPortfolio.addEventListener("click", function(){
     introEl.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;")
-    aboutMeCard.setAttribute("class","card");
+    
     setTimeout(function(){
         introEl.setAttribute("class","no-display");
-    navEl.setAttribute("class","mb-auto visible");
-
-    aboutMeCard.setAttribute("style", "width: 100%; transition-property: transform; transition-duration: 600ms; transform: translate(0%)");
-    // aboutMeCard.setAttribute("class","card");
-    cardTitle.textContent = "Leigh Dahlin"
-    var imgEl = document.createElement("img");
-    imgEl.setAttribute("src","./Images/profile-pic (2).jpg");
-    imgEl.setAttribute("id","about-me-pic")
-    imgEl.setAttribute("alt","Picture of Leigh Dahlin");
-    cardImage.appendChild(imgEl);
-    var paragraphEl = document.createElement("p");
-    paragraphEl.textContent = "My name is Leigh Dahlin and I currently reside in Sacramento, California. I grew up in Houston, Texas and got my bachelor's and master's degrees in Accounting at Texas A&M University in 2014. I worked in the accounting field for seven years when I decided that I needed a career change to something that I was passionate about. That's when I discovered the world of coding and all of the opportunites that came along with it."
-    cardContentEl.appendChild(paragraphEl);
-
-},650)
+        aboutMeCard.setAttribute("class","card");
+        navEl.setAttribute("class","mb-auto visible");
+        aboutMeCard.setAttribute("class","card");
+        setTimeout(function(){
+            aboutMeCard.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:10;");
+        }, 50)
+        
+    },650)
        
 })
 
 workEl.addEventListener("click", function(){
-    aboutMeCard.setAttribute("style","transition-property: transform; transition-duration: 700ms; transform: translate(-120%)");
-    
+    aboutMeCard.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;");
+    contactCard.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;");
     setTimeout(function(){
-        aboutMeCard.setAttribute("class","no-display")
-        workSectionEl.setAttribute("class","work visible")
+        aboutMeCard.setAttribute("class","no-display");
+        contactCard.setAttribute("class","no-display");
+        workSectionEl.setAttribute("class","work visible");
         setTimeout(function(){
-            workSectionEl.setAttribute("style","transition-property: opacity; transition-duration: 250ms; opacity:10;")
-        },50)
-        
-
-    },200)
-
-
+            workSectionEl.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:10;")
+        }, 50)
+    },650)
     
 });
 
+contactEl.addEventListener("click", function(){
+    workSectionEl.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;")
+    aboutMeCard.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:0;");
+    setTimeout(function(){
+        workSectionEl.setAttribute("class", "no-display")
+        aboutMeCard.setAttribute("class","no-display");
+        contactCard.setAttribute("class","card");
+        setTimeout(function(){
+            contactCard.setAttribute("style","transition-property: opacity; transition-duration: 600ms; opacity:10;")
+        }, 50)
+    },650)
+ 
+})
